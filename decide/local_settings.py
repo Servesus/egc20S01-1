e52efc3cab078ca93fac88ce983ba6eafef8165e
Servesus <jesusgarrocho1998@gmail.com>
2020-09-07 18:06:22 +0200

    Issue #1 corregida

---
+++ decide/local_settings.py
@@ -0,0 +1,42 @@
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+APIS = {
+    'authentication': 'http://127.0.0.1',
+    'base': 'http://127.0.0.1',
+    'booth': 'http://127.0.0.1',
+    'census': 'http://127.0.0.1',
+    'mixnet': 'http://127.0.0.1',
+    'postproc': 'http://127.0.0.1',
+    'store': 'http://127.0.0.1',
+    'visualizer': 'http://127.0.0.1',
+    'voting': 'http://127.0.0.1',
+}
+
+BASEURL = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jesgarjim5',
+        'USER': 'decide',
+	'PASSWORD':'decide',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
